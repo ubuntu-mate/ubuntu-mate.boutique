69378d975c78178b6ff4e32e7074db4cd58d559e
Luke Horwell <luke@ubuntu-mate.org>
2019-03-27 21:22:51 +0000

    Drop Analytics.

--- conf.py
+++ conf.py
@@ -1111,19 +1111,7 @@ EXTRA_HEAD_DATA = """
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-BODY_END = """
-    <!-- Google Analytics -->
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-52515755-1', 'auto');
-      ga('require', 'displayfeatures');
-      ga('send', 'pageview');
-    </script>
-"""
+BODY_END = ""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
